a9143157ed82fc3d59ae93afc13f97fcc125c7aa
Albin Varghese <albin8285@gmail.com>
2021-05-17 19:57:48 +0530

    include missing user model

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -11,6 +11,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
+use App\Models\User;
 
 class FortifyServiceProvider extends ServiceProvider
 {
